07414373acd7a0a236a4f38ed69ff3e010921f6b
Lucas <lucasmercado101@gmail.com>
2020-10-02 17:53:45 -0300

    notes can be edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,45 @@
 import React, { useReducer } from "react";
 import Note from "./components/Note";
-// import { Transition } from "react-transition-group";
+import { note } from "./components/types";
+import { FaPlus } from "react-icons/fa";
 import "./assets/main.css";
 
-{
-  // const defaultStyle = {
-  //   transition: "transform 350ms, opacity 350ms, max-height 1750ms",
-  // };
-  // const transitionStyles: any = {
-  //   entering: {
-  //     transform: "translateY(-25%)",
-  //     opacity: 0,
-  //   },
-  //   entered: { transform: "translateY(0)", opacity: 1 },
-  //   exiting: { transform: "translateX(-100%)", opacity: 0 },
-  // };
-}
-
-{
-  /* <Transition
-  in={entered}
-  timeout={{
-    appear: 1000,
-    enter: 0,
-    exit: 350,
-  }}
-  unmountOnExit
->
-  {(state) => (
-    <div
-      style={{
-        ...defaultStyle,
-        ...transitionStyles[state],
-      }}
-    >
-      <Note />
-    </div>
-  )}
-</Transition> */
-}
-
 type action = {
-  type: string;
+  type: number;
   noteID?: number;
+  editedMessage?: note;
 };
 
-type note = {
-  content: string;
-  id: number;
-};
+enum actionTypes {
+  DELETE_NOTE,
+  ADD_NOTE,
+  EDITED_NOTE,
+}
 
 type State = note[];
 
 const reducer = (state: State, action: action): State => {
   switch (action.type) {
-    case "DELETE_NOTE":
+    case actionTypes.DELETE_NOTE:
       return state.filter((i) => i.id !== action.noteID);
-    case "ADD_NOTE":
+    case actionTypes.ADD_NOTE:
       return [
         ...state,
         // pseudo unique id
-        { content: "! New note", id: Math.ceil(Math.random() * 150) },
+        { text: "! New note\n", id: Math.ceil(Math.random() * 150) },
       ];
+
+    case actionTypes.EDITED_NOTE:
+      // console.log(action.editedMessage);
+      const editedID = action.editedMessage!.id;
+
+      return state.map((i) => {
+        if (i.id === editedID) {
+          return action.editedMessage!;
+        } else {
+          return i;
+        }
+      });
 
     default:
       return state;
@@ -71,7 +49,7 @@ const reducer = (state: State, action: action): State => {
 function App() {
   const [notes, dispatch] = useReducer(reducer, [
     {
-      content:
+      text:
         "! Title\nThis __is  underlined     __, and this is ~~crossed out~~, this is\n ''bold''",
       id: 1,
     },
@@ -79,23 +57,27 @@ function App() {
   return (
     <div className="App p-4 w-full text-gray-900">
       <div className="grid items-start auto-grow-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-3 md:gap-4 lg:gap-6 justify-center mb-6">
-        {notes.map(({ content, id }) => {
+        {notes.map((data) => {
           return (
             <Note
-              key={id}
-              onRequestDelete={() =>
-                dispatch({ type: "DELETE_NOTE", noteID: id })
+              key={data.id}
+              onEdited={(editedMessage: note) =>
+                dispatch({ type: actionTypes.EDITED_NOTE, editedMessage })
               }
-              content={content}
+              onRequestDelete={() =>
+                dispatch({ type: actionTypes.DELETE_NOTE, noteID: data.id })
+              }
+              content={data}
             />
           );
         })}
       </div>
       <button
-        onClick={() => dispatch({ type: "ADD_NOTE" })}
-        className="ml-auto mr-auto block w-full lg:w-3/4 xl:w-2/4 rounded hover:b bg-teal-300 p-2 text-lg"
+        onClick={() => dispatch({ type: actionTypes.ADD_NOTE })}
+        style={{ right: "20px", bottom: "20px" }}
+        className="fixed rounded-full bg-teal-300 h-16 w-16 md:h-20 md:w-20 grid place-items-center"
       >
-        New Note
+        <FaPlus className="w-3/5  h-auto text-gray-900 outline-none" />
       </button>
     </div>
   );
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,101 +1,200 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import { note } from "./types";
 import {
   FaPen as EditIcon,
   FaTrashAlt as DeleteIcon,
   FaCheck as ConfirmIcon,
   FaTimes as XIcon,
+  FaEye as ShowIcon,
+  FaEyeSlash as HideIcon,
 } from "react-icons/fa";
 
+enum actionTypes {
+  SET_ROWS,
+  TOGGLE_PARSED_TEXT,
+  SAVED_NOTE_EDIT,
+  TOGGLE_NOTE_EDIT,
+  CANCEL_EDIT,
+  SET_EDIT_TEXT,
+}
+
 type Props = {
-  content: string;
+  content: note;
   onRequestDelete: () => void;
+  onEdited: (note: note) => void;
 };
 
-const EditNote: React.FC<Props> = ({ content, onRequestDelete }) => {
-  const [editing, setEditing] = useState(false);
-  const [textContent, setTextContent] = useState(content);
-  const [rows, setRows] = useState(content.split("\n").length);
+const markedDownContent = (content: string) => {
+  content = content.replace(
+    /!!![ ].+[ ]?/g,
+    (x) => `<h3 class="text-xl">${x.substr(3, x.length)}</h1>`
+  );
 
-  const markedDownContent = (content: string): JSX.Element => {
-    content = content.replace(
-      /![ ].+[ ]?.\n/,
-      (x) => `<h1 class="text-3xl">${x.substr(1, x.length)}</h1>`
-    );
+  content = content.replace(
+    /!![ ].+[ ]?/g,
+    (x) => `<h2 class="text-2xl">${x.substr(2, x.length)}</h1>`
+  );
 
-    content = content.replace(
-      /~~.+~~/,
-      (x) => `<span class="line-through">${x.substr(2, x.length - 4)}</span>`
-    );
+  content = content.replace(
+    /![ ].+[ ]?/g,
+    (x) => `<h1 class="text-3xl">${x.substr(1, x.length)}</h1>`
+  );
 
-    content = content.replace(
-      /__.+__/,
-      (x) =>
-        `<span class="underline">${x.trim().substr(2, x.length - 4)}</span>`
-    );
+  content = content.replace(
+    /~~.+~~/g,
+    (x) => `<span class="line-through">${x.substr(2, x.length - 4)}</span>`
+  );
 
-    content = content.replace(
-      /''.+''/,
-      (x) =>
-        `<span class="font-bold">${x.trim().substr(2, x.length - 4)}</span>`
-    );
+  content = content.replace(
+    /__.+__/g,
+    (x) => `<span class="underline">${x.trim().substr(2, x.length - 4)}</span>`
+  );
 
-    content = content.replace("\n", "<br>");
+  content = content.replace(
+    /''.+''/g,
+    (x) => `<span class="font-bold">${x.trim().substr(2, x.length - 4)}</span>`
+  );
 
-    return (
-      <>
-        <div dangerouslySetInnerHTML={{ __html: content }}></div>
-      </>
-    );
-  };
+  content = content.replace(/<script>.*(<\/script>)?/g, (x) => "");
+
+  content = content.replace(/\r\n|\r|\n/g, "<br>");
 
   return (
     <>
-      {!editing ? (
-        <div className="note shadow rounded-sm p-5 border text-gray-900 w-full">
-          <div className="buttons flex flex-row justify-start items-center mb-2">
-            <button className="edit h-8 hover:text-blue-600 cursor-pointer inline-block">
-              <EditIcon
-                className="h-full w-full"
-                onClick={() => setEditing(true)}
-              />
-            </button>
-            <button
-              className="delete h-8 hover:text-red-600 cursor-pointer ml-3"
-              onClick={onRequestDelete}
-            >
-              <DeleteIcon className="h-full w-full" />
-            </button>
-          </div>
-          <div className="content font-serif">{markedDownContent(content)}</div>
-        </div>
-      ) : (
-        <div className="note shadow rounded-sm p-5 border text-gray-900 w-full">
-          <div className="buttons flex flex-row justify-end items-center mb-2">
-            <button className="edit h-8 hover:text-green-500 cursor-pointer">
-              <ConfirmIcon className="h-full w-full" />
-            </button>
-            <button className="delete h-8 hover:text-red-600 cursor-pointer ml-3">
-              <XIcon
-                className="h-full w-full"
-                onClick={() => setEditing(false)}
-              />
-            </button>
-          </div>
-          <textarea
-            onChange={(e) => {
-              setRows(e.target.value.split("\n").length);
-              setTextContent(e.target.value);
-            }}
-            className="w-full h-full resize-none bg-gray-100 font-mono"
-            name=""
-            id=""
-            rows={rows + 1}
-            value={textContent}
-          ></textarea>
-        </div>
-      )}
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
     </>
   );
 };
 
-export default EditNote;
+type action = {
+  type: number;
+  text?: string;
+};
+
+type State = {
+  rows: number;
+  isParsedTextVisible: boolean;
+  isEditing: boolean;
+  preEditText: string;
+  noteText: string;
+};
+
+const reducer = (state: State, action: action): State => {
+  switch (action.type) {
+    case actionTypes.SET_ROWS:
+      return { ...state, rows: action.text!.split(/\r\n|\r|\n/g).length + 1 };
+    case actionTypes.TOGGLE_PARSED_TEXT:
+      return { ...state, isParsedTextVisible: !state.isParsedTextVisible };
+    case actionTypes.TOGGLE_NOTE_EDIT:
+      return { ...state, isEditing: true, preEditText: state.noteText };
+    case actionTypes.SAVED_NOTE_EDIT:
+      return {
+        ...state,
+        isEditing: false,
+        preEditText: state.noteText,
+        isParsedTextVisible: true,
+      };
+    case actionTypes.CANCEL_EDIT:
+      return {
+        ...state,
+        isEditing: false,
+        noteText: state.preEditText,
+        isParsedTextVisible: true,
+      };
+    case actionTypes.SET_EDIT_TEXT:
+      return { ...state, noteText: action.text! };
+    default:
+      return state;
+  }
+};
+
+const Note: React.FC<Props> = ({ content, onRequestDelete, onEdited }) => {
+  const initialState: State = {
+    rows: content.text.split("\n").length + 1,
+    isParsedTextVisible: true,
+    isEditing: false,
+    preEditText: content.text,
+    noteText: content.text,
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { rows, isParsedTextVisible, isEditing, noteText } = state;
+
+  return (
+    <div className="shadow hover:shadow-lg rounded-sm p-5 border text-gray-900 w-full">
+      <>
+        <div className="flex flex-row justify-start items-center mb-2">
+          {isEditing ? (
+            <>
+              <button className="h-8 hover:text-green-500 cursor-pointer">
+                <ConfirmIcon
+                  className="h-full w-full"
+                  onClick={() => {
+                    onEdited({ text: noteText, id: content.id });
+                    dispatch({ type: actionTypes.SAVED_NOTE_EDIT });
+                  }}
+                />
+              </button>
+              <button
+                className="h-8 cursor-pointer ml-3 hover:text-orange-700"
+                onClick={() =>
+                  dispatch({ type: actionTypes.TOGGLE_PARSED_TEXT })
+                }
+              >
+                {isParsedTextVisible ? (
+                  <HideIcon className="h-full w-full" />
+                ) : (
+                  <ShowIcon className="h-full w-full" />
+                )}
+              </button>
+              <button className="h-8 hover:text-red-600 cursor-pointer ml-3">
+                <XIcon
+                  className="h-full w-full"
+                  onClick={() => dispatch({ type: actionTypes.CANCEL_EDIT })}
+                />
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="h-8 hover:text-blue-600 cursor-pointer">
+                <EditIcon
+                  className="h-full w-full"
+                  onClick={() =>
+                    dispatch({ type: actionTypes.TOGGLE_NOTE_EDIT })
+                  }
+                />
+              </button>
+              <button
+                className=" h-8 hover:text-red-600 cursor-pointer ml-3"
+                onClick={onRequestDelete}
+              >
+                <DeleteIcon className="h-full w-full" />
+              </button>
+            </>
+          )}
+        </div>
+        {isEditing && (
+          <textarea
+            onChange={(e) => {
+              dispatch({ type: actionTypes.SET_ROWS, text: e.target.value });
+              dispatch({
+                type: actionTypes.SET_EDIT_TEXT,
+                text: e.target.value,
+              });
+            }}
+            className="p-2 w-full h-full resize-none bg-gray-100 font-mono"
+            name="noteEdit"
+            id="noteEdit"
+            rows={rows}
+            value={noteText}
+          ></textarea>
+        )}
+        {isEditing && isParsedTextVisible && <hr className="my-4" />}
+        {isParsedTextVisible && (
+          <div className="font-serif">{markedDownContent(noteText)}</div>
+        )}
+      </>
+    </div>
+  );
+};
+
+export default Note;
